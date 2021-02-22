50a6a5c817c3fe8c27be84b51ca542f114f9f5d0
egor.egorov <egor.egorov@xero.com>
2021-02-23 08:41:00 +1300

    Changed isDeviceSecured to check on the versions pre 23 if device can be secured with pattern

--- android/src/main/kotlin/com/ceylonlab/is_device_secure/IsDeviceSecurePlugin.kt
+++ android/src/main/kotlin/com/ceylonlab/is_device_secure/IsDeviceSecurePlugin.kt
@@ -15,43 +15,64 @@ import io.flutter.plugin.common.PluginRegistry.Registrar
 
 
 /** IsDeviceSecurePlugin */
-public class IsDeviceSecurePlugin: FlutterPlugin, MethodCallHandler {
-  
-  private lateinit var channel : MethodChannel
-  private lateinit var applicationContext:Context;
+public class IsDeviceSecurePlugin : FlutterPlugin, MethodCallHandler {
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    applicationContext = flutterPluginBinding.applicationContext;
-    channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "is_device_secure")
-    channel.setMethodCallHandler(this);
-  }
+    private lateinit var channel: MethodChannel
+    private lateinit var applicationContext: Context;
 
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "is_device_secure")
-      channel.setMethodCallHandler(IsDeviceSecurePlugin())
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        applicationContext = flutterPluginBinding.applicationContext;
+        channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "is_device_secure")
+        channel.setMethodCallHandler(this);
     }
-  }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    }else if(call.method == "isSecure"){
-      result.success(isDeviceSecured ());
-    } else {
-      result.notImplemented()
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val channel = MethodChannel(registrar.messenger(), "is_device_secure")
+            channel.setMethodCallHandler(IsDeviceSecurePlugin())
+        }
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else if (call.method == "isSecure") {
+            result.success(isDeviceSecured());
+        } else {
+            result.notImplemented()
+        }
+    }
 
-  private fun isDeviceSecured(): Boolean {
-        val keyguardManager = applicationContext.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager //api 16+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    private fun isDeviceSecured(): Boolean {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val keyguardManager = getSystemService(KeyguardManager::class.java)
             keyguardManager.isDeviceSecure
-        } else keyguardManager.isKeyguardSecure
+        } else {
+            // keyguardManager.isKeyguardSecure "returns true if a PIN, pattern or password is set or a SIM card is locked."
+            // We need to ignore the "SIM locked" bit, we can do this using the DevicePolicyManager
+            // We also don't need to worry about Biometrics as support for these wasn't added until Marshmallow
+            val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+            if (!keyguardManager.isKeyguardSecure) return false
+
+            return when (Settings.Secure.getInt(contentResolver, PASSWORD_TYPE_KEY, DevicePolicyManager.PASSWORD_QUALITY_SOMETHING)) {
+                DevicePolicyManager.PASSWORD_QUALITY_SOMETHING -> { // Pattern
+                    Settings.Secure.getInt(contentResolver, Settings.Secure.LOCK_PATTERN_ENABLED, 0) == 1
+                }
+                DevicePolicyManager.PASSWORD_QUALITY_ALPHANUMERIC, // Password
+                DevicePolicyManager.PASSWORD_QUALITY_ALPHABETIC, // Password
+                DevicePolicyManager.PASSWORD_QUALITY_NUMERIC -> true // PIN
+
+                else -> false
+            }
+        }
+    }
+
+    companion object {
+        private const val PASSWORD_TYPE_KEY = "lockscreen.password_type"
     }
 }
